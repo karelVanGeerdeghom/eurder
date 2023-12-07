a0cdbb9b2d894e6e3fc7910115f76fd2b3b30f7f
Karel Van Geerdeghom <karel.van.geerdeghom@gmail.com>
2023-12-07 15:43:27 +0100

    WIP

--- src/main/java/com/switchfully/eurder/dto/DuplicateOrderDto.java
+++ src/main/java/com/switchfully/eurder/dto/DuplicateOrderDto.java
@@ -1,6 +1,5 @@
 package com.switchfully.eurder.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
@@ -8,12 +7,6 @@ import java.time.LocalDate;
 public class DuplicateOrderDto {
     @NotNull
     private LocalDate orderDate;
-
-    public DuplicateOrderDto() {}
-
-    public DuplicateOrderDto(LocalDate orderDate) {
-        this.orderDate = orderDate;
-    }
 
     public LocalDate getOrderDate() {
         return orderDate;
